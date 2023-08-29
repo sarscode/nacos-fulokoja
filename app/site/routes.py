192b4482fbd0b3592365df3611d2f93b345d3a87
Salifu Sani Rich <salifusanirich@gmail.com>
2023-08-29 22:38:09 +0100

    feat: [WIP] Add subscribe route

--- app/site/routes.py
+++ app/site/routes.py
@@ -7,5 +7,13 @@ from app.site.forms import SubscribeForm
 def home():
     form = SubscribeForm()
     if form.validate_on_submit():
-        return redirect(url_for('site_blueprint.home'))
+        return redirect(url_for('site_blueprint.subscribe'))
     return render_template('site/index.html', form=form)
+
+
+@blueprint.route('/subscribe', methods=['POST'])
+def subscribe():
+    form = SubscribeForm()
+    if form.validate_on_submit():
+        print(form.email_address.data)
+    return redirect(url_for('site_blueprint.home'))
